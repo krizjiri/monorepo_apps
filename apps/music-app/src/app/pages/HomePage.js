7b85392201fdde51fa72f222781a83eed4e73219
Jiří Kříž <kriz.jiri3@gmail.com>
2021-03-21 18:29:52 +0100

    remove source maps

--- apps/music-app/src/app/pages/HomePage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { HomeTemplate } from '@monorepo-test/shared/frontend/ui';
-const HomePage = () => {
-    return (React.createElement(HomeTemplate, { media: "music", pageTitle: 'Music', searchTitle: 'Search music' }));
-};
-export { HomePage };
-//# sourceMappingURL=HomePage.js.map
